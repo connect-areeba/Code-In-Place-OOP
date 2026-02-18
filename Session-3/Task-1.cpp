11feebd6f03f6cda54dbe9ef4b41b458bc5e1c85
Areeba <connect.areeba@gmail.com>
2026-02-18 23:25:26 +0500

    Add DynamicArray class with memory management
    Implemented a DynamicArray class with constructors, destructor, and operator overloading.

--- Session-3/Task-1.cpp
+++ Session-3/Task-1.cpp
@@ -1,1 +1,58 @@
 
+#include <iostream>
+using namespace std;
+
+class DynamicArray {
+private:
+    int* data;
+    int size;
+
+public:
+    DynamicArray() {
+        size = 0;
+        data = NULL;
+    }
+
+    DynamicArray(int n) {
+        size = n;
+        data = new int[size];
+        for (int i = 0; i < size; i++) {
+            data[i] = 0;
+        }
+    }
+
+    DynamicArray(const DynamicArray& other) {
+        size = other.size;
+        data = new int[size];
+        for (int i = 0; i < size; i++) {
+            data[i] = other.data[i];
+        }
+    }
+
+    ~DynamicArray() {
+        delete[] data;
+    }
+
+    int& operator[](int index) {
+        return data[index];
+    }
+
+    int getSize() const {
+        return size;
+    }
+};
+
+int main() {
+    DynamicArray a(2);
+    a[0] = 10;
+    a[1] = 20;
+
+    DynamicArray b = a;   
+    b[0] = 50;
+
+    cout << "a[0] = " << a[0] << endl;  
+    cout << "b[0] = " << b[0] << endl;  
+
+    return 0;
+}
+
